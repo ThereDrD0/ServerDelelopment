242a841d15cc38466260002938edb1f9bcf3e023
ThereDrD <drdtheuser@gmail.com>
2026-03-18 11:30:11 +0300

    add: 3

---
+++ One.Task3/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/calculate", (Requirement req) =>
+{
+    return new { result = req.Num1 + req.Num2 };
+});
+
+app.Run();
+
+internal record struct Requirement(int Num1, int Num2);
