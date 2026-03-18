d934c8530d7d0cabb8d4487ca8f7945c3a0b5c02
ThereDrD <drdtheuser@gmail.com>
2026-03-18 11:34:36 +0300

    add: usings

--- One.Task2/Program.cs
+++ One.Task2/Program.cs
@@ -1,3 +1,9 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
