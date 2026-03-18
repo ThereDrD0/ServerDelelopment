a891c2adba532e4588a957b118c7c581a0735633
ThereDrD <drdtheuser@gmail.com>
2026-03-18 11:17:22 +0300

    add: second

---
+++ One.Task2/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () =>
+{
+    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "index.html");
+    return Results.File(filePath, contentType: "text/html; charset=utf-8");
+});
+
+app.Run();
+
