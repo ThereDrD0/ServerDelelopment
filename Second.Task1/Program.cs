b4ce2f514d1e284eb3dcca08ab08c1de8f2eaefb
ThereDrD <drdtheuser@gmail.com>
2026-04-01 13:38:13 +0300

    add: task 1

---
+++ Second.Task1/Program.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
---
+++ Second.Task1/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Second.Task1;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{   
+    [HttpPost("create_user")]
+    public IActionResult CreateUser([FromBody] User user)
+    {
+        return Ok(user);
+    }
+}
+
+public record User(
+    [Required] string Name,
+    [Required, EmailAddress] string Email,
+    [Range(1, 100)] int? Age = null,
+    bool? IsSubscribed = null
+);
